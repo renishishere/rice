d1f073328514bf452f0987db3c517db472d7aa78
renishishere <71650468+renishishere@users.noreply.github.com>
2023-08-07 22:03:17 -0400

    adding vim to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_config theme choose "ayu Dark"
     alias v="nvim"
+    fish_vi_key_bindings
+    bind -M insert -m default kj 'commandline -f repaint'
 end
